0d05d3d3a7a7ebb21a59d61cc404d0145ce5b1cb
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-12-10 22:47:46 +0000

    add daisy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
   },
   css: [
     '@unocss/reset/tailwind.css',
+    '@kidonng/daisyui/index.css',
   ],
   colorMode: {
     classSuffix: '',
--- unocss.config.ts
+++ unocss.config.ts
@@ -8,15 +8,16 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
+import presetDaisy from 'unocss-preset-daisy'
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['test', 'text-red-500 text-20'],
   ],
   presets: [
     presetUno(),
     presetAttributify(),
+    presetDaisy(),
     presetIcons({
       scale: 1.2,
     }),
