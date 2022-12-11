3fe9b7fc180a505676baefe0227956eca22fbcb1
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-12-11 16:35:37 +0000

    Navigation stuff

--- components/Layout-header.vue
+++ components/Layout-header.vue
@@ -17,9 +17,9 @@
             <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
           </a>
           <ul class="rounded-box bg-base-100 p-2 !rounded-t-0" shadow-black shadow-2xl>
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-            <li><a>Submenu 3</a></li>
+            <li><a>Development</a></li>
+            <li><a>Design</a></li>
+            <li><a>Social media</a></li>
           </ul>
         </li>
       </ul>
