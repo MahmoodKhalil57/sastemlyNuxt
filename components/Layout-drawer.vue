3cbd2f8f0d6f13369f064defa63e971d93aadee6
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-12-18 00:31:34 +0000

    add collapse move drawer services tab

--- components/Layout-drawer.vue
+++ components/Layout-drawer.vue
@@ -10,19 +10,28 @@
         <!-- Sidebar content here -->
         <li><a>Home</a></li>
         <li><a>About</a></li>
-        <li><a>Projects</a></li>
-        <li tabindex="0">
-          <a rounded-t-md>
+        <div class="collapse collapse-arrow">
+          <input id="collapse-input" type="checkbox">
+          <div class="collapse-title font-medium">
             Services
-            <i i-material-symbols-arrow-forward-ios-rounded />
-          </a>
-          <ul class="rounded-box bg-base-100 p-2 !rounded-l-0">
-            <li><a>Development</a></li>
-            <li><a>Design</a></li>
-            <li><a>Social media</a></li>
-          </ul>
-        </li>
+          </div>
+          <div class="collapse-content">
+            <ul class="rounded-box bg-base-100 p-2 !rounded-l-0">
+              <li><a>Development</a></li>
+              <li><a>Design</a></li>
+              <li><a>Social media</a></li>
+            </ul>
+          </div>
+        </div>
+        <li><a>Projects</a></li>
       </ul>
     </template>
   </DaisyDrawercontainer>
 </template>
+
+<style scoped>
+#collapse-input:hover + .collapse-title{
+  background-color: hsla(var(--bc),var(--un-bg-opacity));
+  --un-bg-opacity: 0.1;
+}
+</style>
